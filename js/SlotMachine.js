80962b873e73438ccab2d466423cc9ab6ae5505e
frenkie <frank@frankbosma.nl>
2017-04-26 17:25:06 +0200

    teams are loaded async

--- js/SlotMachine.js
+++ js/SlotMachine.js
@@ -3,9 +3,6 @@ import Vue from 'vue';
 import TeamSelect from 'slotmachine/js/TeamSelect.js';
 import Machine from 'slotmachine/js/Machine.js';
 
-import teamData from 'slotmachine/teams.json!'
-
-
 var SlotMachine = Vue.extend({
 
     data: function () {
